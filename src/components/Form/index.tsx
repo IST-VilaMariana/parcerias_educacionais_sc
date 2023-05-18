47bc60e441d44d313de5375a468bbea259622848
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2023-05-18 09:03:00 -0300

    refactor: add form in sharePoint

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -4,7 +4,7 @@ export function Form(){
     return (
         <iframe
             className={styles.formIframe}
-            src="https://forms.office.com/Pages/ResponsePage.aspx?id=3qZIQdENBE2kxXjjdOT21uP3CeRTan5AhCePMRPro9tUNzdEVVdXUzNSSlpLUjUyRE5ROTlFNzNJOC4u"
+            src="https://forms.office.com/r/WZ28rXpcvW"
             frameBorder="0"
             allowFullScreen
         >
