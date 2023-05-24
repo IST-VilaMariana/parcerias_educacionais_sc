ba5720330710de250572fc27c1678960c435700b
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2023-05-24 13:53:57 -0300

    refactor: corrigindo url facebook

--- src/components/MenuSocialMediaFooter/index.tsx
+++ src/components/MenuSocialMediaFooter/index.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 export function MenuSocialMediaFooter(){
     return (
         <div className={styles.navConteinerFooter}>
-            <a aria-label='Perfil no Facebook' href="https://www.facebook.com/www.facebook.com/senaitocantins" ><ImFacebook /></a>
+            <a aria-label='Perfil no Facebook' href="https://www.facebook.com/senaitocantins" ><ImFacebook /></a>
             <a aria-label='Página do Twitter' href="http://twitter.com/sistemafieto" ><ImTwitter /></a>
             <a aria-label='Canal no Youtube' href="https://www.youtube.com/senaitocantinsto" ><ImYoutube /></a>
             <a aria-label='Perfil no Instagram' href="https://www.instagram.com/senaitocantins/" ><ImInstagram /></a>
