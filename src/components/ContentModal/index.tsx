bd7e6455d796addf0b41b7333cdfe48e18868805
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2023-05-29 16:19:53 -0300

    refactor: update contentModal

--- src/components/ContentModal/index.tsx
+++ src/components/ContentModal/index.tsx
@@ -18,18 +18,6 @@ const empresas_mecanica = list_parceiros.mecanica_metalurgica.map((item) => {
     )
 });
 
-const empresas_vestuario = list_parceiros.textil_vestuario.map((item) => {
-    return (
-        <tr key={item.id}>
-            <td style={{'display':'flex', 'justifyContent': 'flex-start'}}>
-                <FaIndustry className={styles.icon} />
-                <div style={{'margin': '1rem'}}>{item.empresa}</div>
-            </td>
-            <td>{item.escola_responsavel}</td>
-        </tr>
-    )
-});
-
 const empresas_alimentos = list_parceiros.alimentos.map((item) => {
     return (
         <tr key={item.id}>
@@ -42,7 +30,7 @@ const empresas_alimentos = list_parceiros.alimentos.map((item) => {
     )
 });
 
-const empresas_quimica = list_parceiros.quimica.map((item) => {
+const empresas_plasticos = list_parceiros.plasticos.map((item) => {
     return (
         <tr key={item.id}>
             <td style={{'display':'flex', 'justifyContent': 'flex-start'}}>
@@ -66,7 +54,19 @@ const empresas_tec_informacao = list_parceiros.tecnologia_informacao.map((item) 
     )
 });
 
-const empresas_logistica = list_parceiros.logistica_transporte.map((item) => {
+const empresas_automotiva = list_parceiros.automotiva.map((item) => {
+    return (
+        <tr key={item.id}>
+            <td style={{'display':'flex', 'justifyContent': 'flex-start'}}>
+                <FaIndustry className={styles.icon} />
+                <div style={{'margin': '1rem'}}>{item.empresa}</div>
+            </td>
+            <td>{item.escola_responsavel}</td>
+        </tr>
+    )
+});
+
+const empresas_refrigeracao = list_parceiros.refrigeracao.map((item) => {
     return (
         <tr key={item.id}>
             <td style={{'display':'flex', 'justifyContent': 'flex-start'}}>
@@ -114,32 +114,19 @@ const empresas_gestao = list_parceiros.gestao.map((item) => {
     )
 });
 
-const empresas_automotiva = list_parceiros.automotiva.map((item) => {
-    return (
-        <tr key={item.id}>
-            <td style={{'display':'flex', 'justifyContent': 'flex-start'}}>
-                <FaIndustry className={styles.icon} />
-                <div style={{'margin': '1rem'}}>{item.empresa}</div>
-            </td>
-            <td>{item.escola_responsavel}</td>
-        </tr>
-    )
-});
-
 export function ContentModal({ area }: ContentModalProps){
 
     function verify_area(area: number){
         return (
             area === 1 ? empresas_mecanica :
-            area === 2 ? empresas_vestuario :
-            area === 3 ? empresas_alimentos:
-            area === 4 ? empresas_quimica :
-            area === 5 ? empresas_tec_informacao :
-            area === 6 ? empresas_logistica : 
+            area === 2 ? empresas_alimentos :
+            area === 3 ? empresas_plasticos:
+            area === 4 ? empresas_tec_informacao :
+            area === 5 ? empresas_automotiva :
+            area === 6 ? empresas_refrigeracao : 
             area === 7 ? empresas_const_civil :
             area === 8 ? empresas_energia :
             area === 9 ? empresas_gestao :
-            area === 10 ? empresas_automotiva :
             'no_empresas'
         )
     }
