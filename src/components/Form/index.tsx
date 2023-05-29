5a9d896af5a3c6e57fcef293476a8299a7822811
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2023-05-29 16:07:55 -0300

    refactor: add url form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -4,7 +4,7 @@ export function Form(){
     return (
         <iframe
             className={styles.formIframe}
-            src="https://forms.office.com/r/WZ28rXpcvW"
+            src="https://forms.office.com/r/z7m0HwxyUm?embed=true"
             frameBorder="0"
             allowFullScreen
         >
