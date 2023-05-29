7250e304a2869d71493e9b8904d5561a9335e17f
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2023-05-29 16:43:04 -0300

    refactor: add types in buttonDownload

--- src/components/ButtonDownload/index.tsx
+++ src/components/ButtonDownload/index.tsx
@@ -1,7 +1,11 @@
 ﻿import styles from "./styles.module.css";
 import { AiOutlineDownload } from "react-icons/ai";
 
-export function ButtonDownload(endereco_url: string){
+type ButtonProps = {
+    endereco_url: string
+}
+
+export function ButtonDownload({ endereco_url}:ButtonProps){
     return (
         <a href={endereco_url} download={'file.pdf'} aria-label={'document'}><AiOutlineDownload className={styles.icon}/></a>
     )
