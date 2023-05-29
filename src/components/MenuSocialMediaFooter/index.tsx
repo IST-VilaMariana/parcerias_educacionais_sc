e298330adb691f1a98cc56d2437ea78b24e94725
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2023-05-29 16:28:51 -0300

    refactor: update menu medias sociais

--- src/components/MenuSocialMediaFooter/index.tsx
+++ src/components/MenuSocialMediaFooter/index.tsx
@@ -1,14 +1,14 @@
-﻿import { ImFacebook, ImTwitter, ImYoutube, ImInstagram } from 'react-icons/im';
+﻿import { ImFacebook, ImTwitter, ImYoutube, ImLinkedin } from 'react-icons/im';
 
 import styles from './styles.module.css';
 
 export function MenuSocialMediaFooter(){
     return (
         <div className={styles.navConteinerFooter}>
-            <a aria-label='Perfil no Facebook' href="https://www.facebook.com/senaitocantins" ><ImFacebook /></a>
-            <a aria-label='Página do Twitter' href="http://twitter.com/sistemafieto" ><ImTwitter /></a>
-            <a aria-label='Canal no Youtube' href="https://www.youtube.com/senaitocantinsto" ><ImYoutube /></a>
-            <a aria-label='Perfil no Instagram' href="https://www.instagram.com/senaitocantins/" ><ImInstagram /></a>
+            <a aria-label='Perfil no Facebook' href="https://web.facebook.com/firjanoficial?_rdc=1&_rdr" ><ImFacebook /></a>
+            <a aria-label='Página do Twitter' href="https://twitter.com/firjan" ><ImTwitter /></a>
+            <a aria-label='Canal no Youtube' href="https://www.youtube.com/user/SistemaFIRJAN" ><ImYoutube /></a>
+            <a aria-label='Perfil no Instagram' href="https://www.linkedin.com/company/firjan/" ><ImLinkedin /></a>
         </div>
     )
 }
