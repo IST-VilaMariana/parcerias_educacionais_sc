08d4fd41b6794816e73f65dda06518461fead290
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2023-05-30 09:08:30 -0300

    refator: add local fonts averta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,25 @@
 import { AppProps } from 'next/app';
+import localFont from '@next/font/local';
 import { useEffect } from 'react';
 import { Footer } from '../src/components/Footer';
 import { Header } from '../src/components/Header';
 import { axeAccessibilityReporter } from '../src/utils/axeAccessibilityReporter';
 import '../styles/globals.css';
 
+const averta = localFont({
+  src: [
+    {
+      path: '../public/averta/averta-regular.otf',
+      weight: '400',
+      style: 'normal'
+    },
+    {
+      path: '../public/averta/averta-semibold.otf',
+      weight: '700',
+      style: 'normal'
+    }
+  ]
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   
@@ -13,11 +28,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   },[]);
   
   return (
-    <>
+    <div className={averta.className}>
       <Header />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </div>
   )
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,7 @@
 export default function MyDocument(){
     return(
         <Html lang="pt-BR">
-            <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous'/>
-                <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
-            </Head>
+            <Head/>
             <body>
                 <Main />
                 <NextScript />
